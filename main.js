5a24a6b8064825d56bab4d8b74818b67f257edef
Denis Mignanelli <denistschurtschenthaler@gmail.com>
2019-07-27 16:27:18 +0200

    Changes to the mainwindow w/Dalura
    Changes made with Dalura
    -removed addWindow
    -Changed some icons behavior
    -started to work on the sidebar

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ app.on("ready", function(){
         frame: false,
         icon: path.join(__dirname, 'img/icons/appIconLight.png'),//change later
         width: 900,
-        'min-height': 500,
-        'min-width': 900
+        'minHeight': 500,
+        'minWidth': 720
     });
     // Load HTML into window
     mainWindow.loadURL(url.format({
@@ -38,32 +38,10 @@ app.on("ready", function(){
     Menu.setApplicationMenu(topMenu)
 });
 
-//AddWindow creation function
-function createAddWindow(){
-    AddWindow = new BrowserWindow({
-        height: 100,
-        width: 340,
-        title: 'Add Reminder',
-        webPreferences: {nodeIntegration: true},
-        frame: false,
-        resizable: false      
-    });
-    AddWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'createToDo.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-    //Window stays open in the background, to optimize I set the window to null after it gets closed
-    AddWindow.on('close', function(){
-        AddWindow = null;
-    })
-}
-
 //catch item:add
 ipcMain.on('item:add',function(e, item){
     console.log(item)
     mainWindow.webContents.send('item:add', item);
-    AddWindow.close();
 });
 //catch addWindow:open
 ipcMain.on('addWindow:open', function(){
@@ -88,26 +66,10 @@ const mainMenuTemplate = [
         label:'File',
         submenu:[
             {
-                accelerator: 'Shift+Enter',
-                label: 'Add Item',
-                click(){
-                    createAddWindow();
-                }
-            },
-            {
                 label: 'Quit',
                 accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q', //shortcut to quit, checks if the app is running on win32 or on darwin(MacOS)
                 click(){
                     app.quit();
-                }
-            },
-            {
-                accelerator: 'escape',
-                label: 'Close window',
-                click(){
-                    if (AddWindow == !null){ //checks if the window is defined
-                        console.log('test')
-                    } 
                 }
             }
         ]
---
+++ mainHtmlScript.js
@@ -0,0 +1,70 @@
+const electron = require('electron');
+const {ipcRenderer} = electron;
+const ul = document.querySelector('ul');
+const remote = require('electron').remote;
+
+let isOpen = false;
+
+ipcRenderer.on('item:add', function(e, item){
+    const li = document.createElement('li');
+    const itemText = document.createTextNode(item);
+    li.appendChild(itemText);
+    ul.appendChild(li);
+});
+
+//Remove Item
+ul.addEventListener('dblclick', removeItem);
+
+function removeItem(e){
+    e.target.remove();
+}
+
+//Substitute to the windows buttons
+document.getElementById("closeApp").addEventListener("click", function(){
+    ipcRenderer.send('closeApp:close', true)
+});
+
+document.getElementById("minimizeApp").addEventListener("click", function (e) {
+    const window = remote.getCurrentWindow();
+    window.minimize(); 
+});
+
+document.getElementById("maximizeApp").addEventListener("click", function (e) {
+    const window = remote.getCurrentWindow();
+    if (!window.isMaximized()) {
+        window.maximize();          
+    } else {
+        window.unmaximize();
+    }
+});
+
+
+document.getElementById('addReminder').addEventListener('click', function(){
+    checkOpen();
+});
+
+function checkOpen(){
+    if (isOpen == false) {
+        // this is '+' button
+            document.getElementById('addItem').style.display='block';
+            document.getElementById('addReminder').style.transform='rotate(45deg)'
+            isOpen = true;
+    } else {
+        // this is '+' button
+            document.getElementById('addItem').style.display='none';
+            document.getElementById('addReminder').style.transform='rotate(0deg)'
+            isOpen = false;
+    }
+}
+
+
+//TODO: rewrite code
+//send item and add it to the main html list
+const form = document.querySelector('form');
+form.addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+    const item = document.querySelector('#item').value;
+    ipcRenderer.send('item:add', item)
+}
