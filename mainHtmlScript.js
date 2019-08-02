74e89e6f617c6434bf8adaa45873083a29259580
Denis Mignanelli <denistschurtschenthaler@gmail.com>
2019-08-02 16:04:17 +0200

    Sidebar functionality
    Lists added to the sidebar now get saved on the local storage.

--- mainHtmlScript.js
+++ mainHtmlScript.js
@@ -5,20 +5,6 @@ const remote = require('electron').remote;
 
 let isOpen = false;
 
-ipcRenderer.on('item:add', function(e, item){
-    const li = document.createElement('li');
-    const itemText = document.createTextNode(item);
-    li.appendChild(itemText);
-    ul.appendChild(li);
-});
-
-//Remove Item
-ul.addEventListener('dblclick', removeItem);
-
-function removeItem(e){
-    e.target.remove();
-}
-
 //Substitute to the windows buttons
 document.getElementById("closeApp").addEventListener("click", function(){
     ipcRenderer.send('closeApp:close', true)
@@ -67,8 +53,69 @@ form.addEventListener('submit', submitForm);
 
 function submitForm(e){
     e.preventDefault();
-    const item = document.querySelector('#item').value;
-    ipcRenderer.send('item:add', item)
 }
 
+//looks if a List in the sidebar is hovered
+//Not finished
+
 //Sidebar functionality
+const listsContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+
+const LOCAL_STORAGE_LIST_KEY = 'reminder.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'reminder.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+listsContainer.addEventListener('click', function(e){
+    if (e.target.tagName.toLowerCase() === 'li'){
+        selectedListId = e.target.dataset.listId
+    }
+    saveAndRender()
+})
+
+newListForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    const listName = newListInput.value
+    if (listName == null || listName === ''){ return }
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    saveAndRender()
+})
+
+function createList(name) {
+    return {id: Date.now().toString(), name: name, tasks: []}
+}
+
+function saveAndRender(){
+    save()
+    render()
+}
+
+function save(){
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+}
+
+function render() {
+    ClearElement(listsContainer)
+    lists.forEach(list =>{
+        const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
+        listElement.classList.add('list-item')
+        listElement.innerText = list.name
+        if (list.id === selectedListId){
+            listElement.classList.add('active-list')
+        }
+        listsContainer.appendChild(listElement)
+    })
+}
+
+function ClearElement(element){
+    while (element.firstChild){
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
